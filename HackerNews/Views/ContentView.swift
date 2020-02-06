ca39c1a656a3bcd2a57ab54f711f0b6639417a9c
Sonia Puertas <epuertas@ucdavis.edu>
2020-02-05 17:20:13 -0800

    update URL links

---
+++ HackerNews/Views/ContentView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url:post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                    
+                }
+                
+                
+            }
+                
+            .navigationBarTitle("Hacker News")
+        }
+            /* viewLoad */
+            .onAppear {
+                self.networkManager.fetchData()
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ HackerNews/Views/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Sonia Puertas Acosta on 2/5/20.
+//  Copyright © 2020 Sonia Puertas Acosta. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    let url: String?
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
+struct WebView: UIViewRepresentable {
+    let urlString: String?
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+        
+    
+}
